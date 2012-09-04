fd2e24618a4d4a66ecbc149851cf47a7b4e510e7
meadlai <laiqinyi@gmail.com>
2012-09-04 18:56:11 +0800

    nothing

--- projects/OC_Basic/OC_Basic/CatClass.h
+++ projects/OC_Basic/OC_Basic/CatClass.h
@@ -8,6 +8,29 @@
 
 #import <Foundation/Foundation.h>
 
-@interface CatClass : NSObject
+@interface CatClass : NSObject{
+    NSString* _name;
+    NSString* _skin;
+    NSString* _gender;
+    NSInteger _age;
+}
+@property(nonatomic,copy) NSString* name;
+@property(nonatomic,retain) NSString* skin;
+@property(nonatomic,copy) NSString* gender;
+@property(nonatomic) NSInteger age;
+
+-(id) initCat:(NSString*) iName withAge:(NSInteger) iAge;
+
+//init初始化函数
+-(id) init:(NSString *)iName withAge:(NSInteger)iAge withGender:(NSString*)sGender withSkin:(NSString*)iSkin;
+
+//吃东西
+-(void) eat:(NSString*) ifoodName;
+
+//睡觉
+-(void) sleep;
+
+//公共方法
++(void)breath;
 
 @end
