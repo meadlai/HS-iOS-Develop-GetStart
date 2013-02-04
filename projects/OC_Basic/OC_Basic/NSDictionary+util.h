9c120dbe2fa6f67328370cef76142b43cf62cb02
meadlai <laiqinyi@gmail.com>
2013-02-04 14:30:25 +0800

    add task.

---
+++ projects/OC_Basic/OC_Basic/NSDictionary+util.h
@@ -0,0 +1,13 @@
+//
+//  NSDictionary+util.h
+//  OC_Basic
+//
+//  Created by Mead on 2/4/13.
+//  Copyright (c) 2013 Mead. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (util)
+-(void)print:(NSObject*)obj;
+@end
---
+++ projects/OC_Basic/OC_Basic/NSDictionary+util.m
@@ -0,0 +1,15 @@
+//
+//  NSDictionary+util.m
+//  OC_Basic
+//
+//  Created by Mead on 2/4/13.
+//  Copyright (c) 2013 Mead. All rights reserved.
+//
+
+#import "NSDictionary+util.h"
+
+@implementation NSDictionary (util)
+-(void)print:(NSObject*)obj{
+    NSLog(@"print something");
+}
+@end
